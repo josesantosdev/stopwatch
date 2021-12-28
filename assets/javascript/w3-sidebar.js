cc7d9cb0c4da7691ee159650c9bb8ad1de64273a
thecaasantos <thecaasantos@gmail.com>
2021-12-28 11:42:41 -0300

    Menu tela inteira

--- assets/javascript/w3-sidebar.js
+++ assets/javascript/w3-sidebar.js
@@ -1,6 +1,6 @@
         function w3_open() {
-            document.getElementById("main").style.marginLeft = "25%";
-            document.getElementById("mySidebar").style.width = "25%";
+            document.getElementById("main").style.marginLeft = "77%";
+            document.getElementById("mySidebar").style.width = "100%";
             document.getElementById("mySidebar").style.display = "block";
             document.getElementById("openNav").style.display = 'none';
         }
